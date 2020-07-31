9872d111a43fa7ef36dff0d093e43272d6490987
dartrhevan <vovasatunkin@mail.ru>
2020-07-31 21:51:44 +0500

    small theming

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,11 +4,26 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store/store';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core';
+import { red, blue } from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: blue[400],
+        },
+        secondary: {
+            main: red[800],
+        },
+    },
+});
 
 ReactDOM.render(
   <React.StrictMode>
       <Provider store={store}>
-        <App />
+          <ThemeProvider theme={theme}>
+            <App />
+          </ThemeProvider>
       </Provider>
   </React.StrictMode>,
   document.getElementById('root')
